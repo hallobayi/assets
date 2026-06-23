fa30e4e2093a2819a3021389f508085bf69736e9
mdestafadilah <desta.08b@gmail.com>
2026-06-23 13:32:35 +0700

    feat: add kasir.js for discount calculation and keyboard shortcut handling

--- themes/modern/halobayi/js/kasir.js
+++ themes/modern/halobayi/js/kasir.js
@@ -405,11 +405,15 @@ function holdingtransaksi() {
 window.setTimeout("waktu()", 1000);
 
 function waktu() {
-    var waktu = new Date();
-    setTimeout("waktu()", 1000);
-    document.getElementById("jam").innerHTML = waktu.getHours() +
-        ` : `;
-    document.getElementById("menit").innerHTML = waktu.getMinutes() +
-        ` : `;
-    document.getElementById("detik").innerHTML = waktu.getSeconds();
+    var d = new Date();
+    setTimeout(waktu, 1000);
+    var elJam = document.getElementById("jam");
+    var elMenit = document.getElementById("menit");
+    var elDetik = document.getElementById("detik");
+
+    if (elJam && elMenit && elDetik) {
+        elJam.innerHTML = d.getHours() + ` : `;
+        elMenit.innerHTML = d.getMinutes() + ` : `;
+        elDetik.innerHTML = d.getSeconds();
+    }
 }
